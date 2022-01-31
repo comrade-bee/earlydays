f5cb6fc32eb3718d455d13a8e734049a5df33946
comrade_bee <comradebee1@protonmail.com>
2022-01-30 23:44:57 -0600

    New recipes and recipe fixes

--- src/main/java/cafe/ferret/mc/earlydays/block/WheelLoomBlock.java
+++ src/main/java/cafe/ferret/mc/earlydays/block/WheelLoomBlock.java
@@ -1,9 +1,15 @@
 package cafe.ferret.mc.earlydays.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
 
 public class WheelLoomBlock extends Block {
     public WheelLoomBlock(Settings settings) {
         super(settings);
+    }
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
     }
 }
