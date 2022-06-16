dbc7cece5a42d7174009b62eb4c63191e52550cd
comrade_bee <comradebee1@protonmail.com>
2022-06-16 01:45:46 -0500

    FISH TRAP, PART ONE: NEW BEGINNINGS

--- src/main/java/cafe/ferret/mc/earlydays/block/FishTrapBlock.java
+++ src/main/java/cafe/ferret/mc/earlydays/block/FishTrapBlock.java
@@ -1,22 +1,28 @@
 package cafe.ferret.mc.earlydays.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
-import net.minecraft.block.Waterloggable;
+import cafe.ferret.mc.earlydays.block.entity.FishTrapBlockEntity;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,19 +32,22 @@ public class FishTrapBlock extends Block implements Waterloggable {
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    public static final BooleanProperty BAITED = BooleanProperty.of("baited");
+    public static final BooleanProperty FULL = BooleanProperty.of("full");
 
     public FishTrapBlock(Settings settings) {
         super(settings);
+        this.setDefaultState();
     }
 
     protected void setDefaultState() {
-        setDefaultState(stateManager.getDefaultState().with(WATERLOGGED, false));
+        setDefaultState(stateManager.getDefaultState().with(WATERLOGGED, false).with(FULL, false).with(BAITED, false));
     }
     
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext context){
-        return this.getDefaultState().with(FACING, context.getPlayerFacing().getOpposite()).with(WATERLOGGED, false);
+        return this.getDefaultState().with(FACING, context.getPlayerFacing().getOpposite()).with(WATERLOGGED, false).with(BAITED, false).with(FULL,false);
     }
     @Override
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState newState,
@@ -60,8 +69,7 @@ public class FishTrapBlock extends Block implements Waterloggable {
     @Override
     protected void appendProperties (StateManager.Builder<Block, BlockState> builder){
         super.appendProperties(builder);
-        builder.add(FACING);
-        builder.add(WATERLOGGED);
+        builder.add(FACING, WATERLOGGED, BAITED, FULL);
     }
     @Override
     public FluidState getFluidState(BlockState state) {
@@ -72,4 +80,75 @@ public class FishTrapBlock extends Block implements Waterloggable {
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context){
         return TRAP;
     }
+
+    //BLOCK ACTION THINGS
+    @Override
+    public BlockRenderType getRenderType (BlockState state){
+        return BlockRenderType.MODEL;
+    }
+
+    public ActionResult onUse (BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
+        if(!world.isClient){
+
+            if(!state.get(BAITED)&&!state.get(FULL)){}
+
+            if(state.get(FULL)){}
+        }
+
+        if(!world.isClient && hand == Hand.MAIN_HAND){
+            boolean currentState = state.get(BAITED);
+            world.setBlockState(pos, state.with(BAITED, !currentState), 3);
+        }
+        return ActionResult.SUCCESS;
+    }
+
+
+    /*
+        @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(world.isClient()) return ActionResult.CONSUME;
+        BloomeryBlockEntity blockEntity = (BloomeryBlockEntity) world.getBlockEntity(pos);
+        ItemStack cookingItem = blockEntity.getItems().get(0);
+        ItemStack fuelItem = blockEntity.getItems().get(1);
+        ItemStack handStack = player.getStackInHand(hand);
+
+
+        if(fuelItem.isEmpty()) {
+            if(!handStack.isEmpty()) {
+                // if (handStack.getItem() instanceof FuelRegistry) {
+                if (handStack.getItem() == Items.COAL) {
+                    blockEntity.setStack(1, handStack.copy());
+                    blockEntity.getItems().get(0).setCount(1);
+                    handStack.decrement(1);
+                    // blockEntity.markDirty();
+                    world.playSound(null, pos, SoundEvents.ENTITY_ITEM_FRAME_ADD_ITEM, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                }
+            }
+        }
+
+        if(cookingItem.isEmpty()) {
+            if(!handStack.isEmpty()) {
+                blockEntity.setStack(0, handStack.copy());
+                blockEntity.getItems().get(0).setCount(1);
+                handStack.decrement(1);
+                // blockEntity.markDirty();
+                world.playSound(null, pos, SoundEvents.ENTITY_ITEM_FRAME_ADD_ITEM, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            }
+        } else {
+            if(handStack.isEmpty()) {
+                // blockEntity.canDry = false;
+                // blockEntity.checkedRecipe = false;
+                // blockEntity.dryingTime = 0;
+                // blockEntity.markDirty();
+                player.getInventory().offerOrDrop(blockEntity.getStack(0));
+                blockEntity.removeStack(0);
+                world.playSound(null, pos, SoundEvents.ENTITY_ITEM_FRAME_REMOVE_ITEM, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            }
+        }
+        // world.updateListeners(pos, state, state, Block.NOTIFY_ALL);
+        return ActionResult.SUCCESS;
+    }
+
+     */
+
 }
--- src/main/java/cafe/ferret/mc/earlydays/util/LootTableModifier.java
+++ src/main/java/cafe/ferret/mc/earlydays/util/LootTableModifier.java
@@ -2,19 +2,20 @@ package cafe.ferret.mc.earlydays.util;
 
 import cafe.ferret.mc.earlydays.init.EarlyDaysItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
-import net.minecraft.loot.provider.number.LootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 
 public class LootTableModifier {
 
     public static void lootTableModifier(){
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             //Tag<Block> DROPS_FLINT = ServerTagManagerHolder.getTagManager().getOrCreateTagGroup(Registry.BLOCK_KEY).getTag(new Identifier(MODID,"drops_flint"));
-
             //Set<Identifier> DROPS_FLINT_IDS = DROPS_FLINT.values().stream().map(block -> block.getLootTableId()).collect(Collectors.toSet());
 
             if (Blocks.GRASS_BLOCK.getLootTableId().equals(id)) {
